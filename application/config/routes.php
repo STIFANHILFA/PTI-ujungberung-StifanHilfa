77f97a02ee893c61d6999d64b5122c04d4d31a0d
STIFANHILFA <123138933+STIFANHILFA@users.noreply.github.com>
2024-11-06 17:35:11 +0700

    TugasCodingPTI-StifanHilfa

---
+++ application/config/routes.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script accessallowed');
+$route['default_controller'] = "home";
+$route['login'] = 'home/login';
+$route['login_failure'] = 'home/login';
+$route['dashboard'] = 'home';
+$route['item'] = 'newitem';
+$route['model'] = 'stock';
+$route['proses'] = 'mesin';
+$route['monitoring'] = 'monitoring';
+$route['report'] = 'report';
+$route['404_override'] = 'MY_Exceptions/show_404';/* End of file
+routes.php */
+/* Location: ./application/config/routes.php */
